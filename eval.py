fc455123a07081c52357e1911804aeaf948ef1f6
ZLY <953472069@qq.com>
2025-02-19 11:04:50 +0800

    update eval

--- eval.py
+++ eval.py
@@ -4,20 +4,28 @@ from torchvision import models
 import torchvision
 import torchvision.transforms as transforms
 from sklearn.metrics import classification_report, f1_score, accuracy_score
+import os
+import torchvision.datasets as datasets
 
+MODEL_PATH = "saved/resnet18_cifar10_v3.pth"
+
+# load and modify model
 model = models.resnet18(weights=None)
 model.fc = nn.Linear(model.fc.in_features, 10)  # modify fc layer
 model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
 model.maxpool = nn.Identity()
-
-model.load_state_dict(torch.load("saved/resnet18_cifar10_v3.pth"))
-
+model.load_state_dict(torch.load(MODEL_PATH))
 model.eval()
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+# load testset
+if not os.path.exists("saved/cifar10_testset.pth"):
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    torch.save(testset, "saved/cifar10_testset.pth")
+
 
 testset = torch.load("saved/cifar10_testset.pth", weights_only=False)
 testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
